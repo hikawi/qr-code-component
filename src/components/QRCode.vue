c637edf88236312498f018182673705ac8d2508d
tsukimi <128211112+hikawi@users.noreply.github.com>
2024-08-31 02:33:52 +0700

    make the text larger

--- src/components/QRCode.vue
+++ src/components/QRCode.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="bg-white rounded-xl max-w-full w-80 gap-4 font-outfit p-4 pb-8 flex items-center justify-center flex-col text-center shadow-lg"
+    class="bg-white rounded-xl max-w-full w-80 gap-4 font-outfit p-4 pb-8 flex items-center justify-center flex-col text-center shadow-xl"
   >
     <img
       src="/image-qr-code.png"
@@ -8,10 +8,10 @@
       class="rounded-xl w-full h-full"
     />
 
-    <h1 class="font-bold text-xl text-slate-900 leading-tight px-4">
+    <h1 class="font-bold text-xl text-slate-900 leading-tight px-4 selection:text-red-900">
       Improve your front-end skills by building projects
     </h1>
-    <p class="text-sm mx-4 text-slate-500 leading-tight px-4">
+    <p class="mx-4 text-slate-500 leading-tight selection:text-blue-500">
       Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
     </p>
   </div>
